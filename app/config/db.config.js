87435759e237004652c231472f950dbc0293395f
Ludwig Valdenegro <ludwigval@gmail.com>
2023-08-29 01:36:59 -0400

    proyecto casi terminado

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,1 +1,15 @@
 //db_bootcamp
+
+module.exports = {
+    HOST: 'localhost',
+    USER: 'admin',
+    PASSWORD: '123456',
+    DB: 'db_bootcamp',
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
